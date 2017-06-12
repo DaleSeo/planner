47f9512b4903c1e859f7a5f68491aa4470a3284d
Dale Seo <dale.seo@gmail.com>
2017-06-12 13:18:29 +0900

    start home

---
+++ web/src/components/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="ui container">
+    <h1 class="ui dividing header">
+      Dale's Timer
+    </h1>
+    <p>
+      Something
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+div.ui.container {
+  margin-top: 50px;
+}
+</style>
